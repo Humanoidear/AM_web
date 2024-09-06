2d7d899fa73680f7e435d96d1e3ad27020df75f9
Alex <basurillodesgastorito@gmail.com>
2024-09-06 16:28:03 +0200

    Add proper SEO.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'alex malvehy';
-export const SITE_DESCRIPTION = 'Description';
+export const SITE_DESCRIPTION = "A student and self-taught graphic and motion designer from Barcelona who specializes in branding, visual identity, and graphics for television and streaming, continually learning and evolving.";
