d7c3e1e49efd65dc4eef6d2241998c098319c1ae
Alex <basurillodesgastorito@gmail.com>
2024-09-07 15:44:33 +0200

    Fix.

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'alex malvehy';
-export const SITE_DESCRIPTION = "A student and self-taught graphic and motion designer from Barcelona who specializes in branding, visual identity, and graphics for television and streaming, continually learning and evolving.";
